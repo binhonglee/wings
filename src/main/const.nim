a8feebe264043a2226a59663928dabe8ebf10ca9
BinHong Lee <binhong@binhong.me>
2021-01-09 20:13:48 -0800

    Downgrade github ci checkout version to fix documentation

--- src/main/const.nim
+++ src/main/const.nim
@@ -3,5 +3,5 @@
 # This file is generated from `src/main/scripts/preCompile.nims`.
 # If you would like to make any changes, please make your edits there instead.
 
-const VERSION*: string = "v0.0.6-alpha"
-const HASH*: string = "eb7d6492d132d33d81a6ee01c3be37afa11d0a88"
+const VERSION*: string = "v0.0.6-alpha-1-g2ab4cbe"
+const HASH*: string = "2ab4cbef90114d20513dd0fbc1065d0a5127eecb"
