47505ae57673fe12289f3740c52353dd56df2f1c
BinHong Lee <binhong@binhong.me>
2025-02-19 00:44:11 -0800

    Release v0.0.8-alpha

--- src/main/const.nim
+++ src/main/const.nim
@@ -3,5 +3,5 @@
 # This file is generated from `src/main/scripts/preCompile.nims`.
 # If you would like to make any changes, please make your edits there instead.
 
-const VERSION*: string = "v0.0.7-alpha"
-const HASH*: string = "707100f4d7c5ea6f396436a0300d26178b87630c"
+const VERSION*: string = "v0.0.8-alpha"
+const HASH*: string = "3519e13e35caa2aca1d441d8a53be80f117cc0a8"
